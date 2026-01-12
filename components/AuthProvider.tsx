d868c3c965a24db32399dd8638196eb1c2c6241d
danbal6016-collab <danbal6016@gmail.com>
2026-01-12 22:05:28 +0900

    Update AuthProvider.tsx

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "../services/supabase";
 import type { User } from "@supabase/supabase-js";
