06c161a069f91bd4b48403ed2c5ee31bf675730d
danbal6016-collab <danbal6016@gmail.com>
2026-01-14 16:29:57 +0900

    Update AuthCallback.tsx

--- components/AuthCallback.tsx
+++ components/AuthCallback.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { supabase } from "./lib/supabase";
+import { supabase } from "../src/lib/supabase";
+
 
 export default function AuthCallback() {
   useEffect(() => {
